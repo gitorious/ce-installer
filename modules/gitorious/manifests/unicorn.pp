b2e069216ed9c7c54d05a01e56a47d1354524d70
Marius Mathiesen <marius@gitorious.com>
2013-05-13 15:24:53 +0200

    Adding init.d script (=service) for unicorn
    This script works properly with:
    - start
    - stop
    - restart
    - reload
    - chkconfig
    
    It is used from the restart and status commands and in the Monit recipe.

--- modules/gitorious/manifests/unicorn.pp
+++ modules/gitorious/manifests/unicorn.pp
@@ -9,9 +9,22 @@ class gitorious::unicorn {
     content => template("gitorious/unicorn.rb.erb"),
   }
 
+  file { "/etc/init.d/gitorious-unicorn":
+    owner => root,
+    group => root,
+    mode => 0755,
+    require => File["unicorn.rb"],
+    content => template("gitorious/etc/init.d/gitorious-unicorn.erb")
+  }
+
   monit::config{ "unicorn":
     pids_dir => "/var/www/gitorious/app/tmp/pids",
     t_app_root => "/var/www/gitorious/app",
+  }
+  service { "gitorious-unicorn":
+    require => File["/etc/init.d/gitorious-unicorn"],
+    enable => true,
+    ensure => running,
   }
 
   file { "/usr/bin/restart_gitorious":
