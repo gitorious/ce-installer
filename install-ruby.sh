0102d33f49970f478ccb79045715a1c30c0fba45
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2013-07-10 14:31:58 +0200

    Automated installation of ruby-install, chruby, ruby 1.9.3

---
+++ install-ruby.sh
@@ -0,0 +1,26 @@
+yum install -y wget tar make gcc ntp sudo
+
+echo "Setting time to avoid makefile warning..."
+ntpdate pool.ntp.org
+
+echo "Downloading src tarballs..."
+cd /tmp
+wget --no-check-certificate -O ruby-install-0.2.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.2.1.tar.gz
+tar -xzvf ruby-install-0.2.1.tar.gz
+wget --no-check-certificate -O chruby-0.3.6.tar.gz https://github.com/postmodern/chruby/archive/v0.3.6.tar.gz
+tar -xzvf chruby-0.3.6.tar.gz
+
+echo "Setting up ruby-install..."
+su -c 'cd /tmp/ruby-install-0.2.1/ && make install'
+
+echo "Installing ruby 1.9.3..."
+su -c 'ruby-install ruby 1.9.3-p448'
+
+echo "Setting up chruby..."
+su -c 'cd /tmp/chruby-0.3.6/ && make install'
+
+#echo "Set up 1.9.3 as default ruby version"
+#cat << EOF > /etc/profile.d/chruby.sh
+#source /usr/local/share/chruby/chruby.sh
+#chruby 1.9.3-p448
+#EOF
