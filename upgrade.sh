6940cd3274b73648210a0c3d8dec5a807390fafe
Pawel Pierzchala <pawelpierzchala@gmail.com>
2014-03-28 12:11:54 +0100

    Reload monit on upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -45,6 +45,7 @@ install-ruby-19 () {
 
 update-executables-to-use-chruby () {
   ./render_config.rb modules/gitorious/templates/monit.d/unicorn.monit.erb > /etc/monit.d/unicorn.monit
+  monit reload
   ./render_config.rb modules/gitorious/templates/etc/init.d/gitorious-unicorn.erb > /etc/init.d/gitorious-unicorn
   chmod +x /etc/init.d/gitorious-unicorn
 
