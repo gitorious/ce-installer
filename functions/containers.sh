9ce4afe3ccd9fb01a97c17aac6fdc043b9613e2b
Marcin Kulik <m@ku1ik.com>
2014-05-08 16:43:10 +0200

    Make start_containers output more meaningful

--- functions/containers.sh
+++ functions/containers.sh
@@ -2,24 +2,48 @@ start_containers() {
   log "Creating and starting new containers..."
 
   # create data only container that exits immediately, with config and data volumes mapped to host
-  docker run --name gitorious-data -v /etc/gitorious:/srv/gitorious/config -v /var/lib/gitorious:/srv/gitorious/data -v /srv/gitorious/assets busybox /bin/true
+
+  log "  creating gitorious-data..."
+  docker run --name gitorious-data -v /etc/gitorious:/srv/gitorious/config -v /var/lib/gitorious:/srv/gitorious/data -v /srv/gitorious/assets busybox /bin/true >/dev/null
 
   # start containers
-  docker run -d --name gitorious-mysql -v /var/lib/gitorious/mysql:/var/lib/mysql -v /var/log/gitorious/mysql:/var/log/mysql gitorious/mysql
-  docker run -d --name gitorious-redis -v /var/lib/gitorious/redis:/var/lib/redis -v /var/log/gitorious/redis:/var/log/redis gitorious/redis
-  docker run -d --name gitorious-memcached gitorious/memcached
-  docker run -d --name gitorious-git-daemon -p 9418:9418 --volumes-from gitorious-data gitorious/git-daemon
-  docker run -d --name gitorious-postfix gitorious/postfix
+
+  log "  creating gitorious-mysql..."
+  docker run -d --name gitorious-mysql -v /var/lib/gitorious/mysql:/var/lib/mysql -v /var/log/gitorious/mysql:/var/log/mysql gitorious/mysql >/dev/null
+
+  log "  creating gitorious-redis..."
+  docker run -d --name gitorious-redis -v /var/lib/gitorious/redis:/var/lib/redis -v /var/log/gitorious/redis:/var/log/redis gitorious/redis >/dev/null
+
+  log "  creating gitorious-memcached..."
+  docker run -d --name gitorious-memcached gitorious/memcached >/dev/null
+
+  log "  creating gitorious-git-daemon..."
+  docker run -d --name gitorious-git-daemon -p 9418:9418 --volumes-from gitorious-data gitorious/git-daemon >/dev/null
+
+  log "  creating gitorious-postfix..."
+  docker run -d --name gitorious-postfix gitorious/postfix >/dev/null
 
   # update db schema
-  docker run --rm --link gitorious-mysql:mysql --volumes-from gitorious-data gitorious/app bin/rake db:migrate
 
-  # start more containers (which expect db schema to be already there)
-  docker run -d --name gitorious-queue --link gitorious-mysql:mysql --link gitorious-redis:redis --link gitorious-memcached:memcached --link gitorious-postfix:smtp --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/app bin/resque
-  docker run -d --name gitorious-sphinx --link gitorious-mysql:mysql --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/sphinx
-  docker run -d --name gitorious-web --link gitorious-mysql:mysql --link gitorious-redis:redis --link gitorious-memcached:memcached --link gitorious-sphinx:sphinx --link gitorious-postfix:smtp --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/app bin/unicorn
-  docker run -d --name gitorious-sshd --link gitorious-web:web -p 5022:22 --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/sshd
-  docker run -d --name gitorious-nginx --link gitorious-web:web -p 80:80 --volumes-from gitorious-data -v /var/log/gitorious/nginx:/var/log/nginx gitorious/nginx
+  log "  updating database..."
+  docker run --rm --link gitorious-mysql:mysql --volumes-from gitorious-data gitorious/app bin/rake db:migrate >/dev/null
+
+  # start more containers (which expect db schema to be already there + ones depending on them)
+
+  log "  creating gitorious-queue..."
+  docker run -d --name gitorious-queue --link gitorious-mysql:mysql --link gitorious-redis:redis --link gitorious-memcached:memcached --link gitorious-postfix:smtp --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/app bin/resque >/dev/null
+
+  log "  creating gitorious-sphinx..."
+  docker run -d --name gitorious-sphinx --link gitorious-mysql:mysql --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/sphinx >/dev/null
+
+  log "  creating gitorious-web..."
+  docker run -d --name gitorious-web --link gitorious-mysql:mysql --link gitorious-redis:redis --link gitorious-memcached:memcached --link gitorious-sphinx:sphinx --link gitorious-postfix:smtp --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/app bin/unicorn >/dev/null
+
+  log "  creating gitorious-sshd..."
+  docker run -d --name gitorious-sshd --link gitorious-web:web -p 5022:22 --volumes-from gitorious-data -v /var/log/gitorious/app:/srv/gitorious/app/log gitorious/sshd >/dev/null
+
+  log "  creating gitorious-nginx..."
+  docker run -d --name gitorious-nginx --link gitorious-web:web -p 80:80 --volumes-from gitorious-data -v /var/log/gitorious/nginx:/var/log/nginx gitorious/nginx >/dev/null
 }
 
 remove_containers() {
