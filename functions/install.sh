ed1c4cf80d3a7f673f49b51120e4c1696ea0b799
Marcin Kulik <m@ku1ik.com>
2015-01-22 14:00:09 +0100

    Reuse existing host setting when upgrading

--- functions/install.sh
+++ functions/install.sh
@@ -1,9 +1,13 @@
 gitorious_version=${VERSION:-v3.2.0}
 
 install_gitorious() {
-  echo "Following information will be used to generate configuration file and SSL certificate:"
-  read -e -p "  hostname (FQDN): " -i $(hostname -f 2>/dev/null || hostname) gitorious_host
-  echo
+  gitorious_host=$(egrep '^host:' /var/www/gitorious/app/config/gitorious.yml 2>/dev/null | awk '{print $2}')
+
+  if [[ -z $gitorious_host ]]; then
+    echo "Following information will be used to generate configuration file and SSL certificate:"
+    read -e -p "  hostname (FQDN): " -i $(hostname -f 2>/dev/null || hostname) gitorious_host
+    echo
+  fi
 
   install_ansible
 
